fa8e84d18cb3f89b7d75a375035012d99340de75
various.authors <various.authors@gmail.com>
2016-10-11 16:53:03 -0700

    can scroll between tweets

--- Assets/Scripts/MainMonitor.cs
+++ Assets/Scripts/MainMonitor.cs
@@ -1,15 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MainMonitor : MonoBehaviour {
 
+	public List<RectTransform> tweets;
+	public RectTransform feed;
+
+	private float tweetHeight;
+	private int position = 0; // which item are we "at"
+
 	// Use this for initialization
 	void Start () {
-	
+		tweetHeight = tweets [0].rect.height;
+
+		// set the position of each tweet in the feed
+		for (int i = 0; i < tweets.Count; i++) {
+			tweets [i].anchoredPosition += new Vector2 (0, i * -tweetHeight);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			position = Mathf.Min (position + 1, tweets.Count - 1);
+
+			StopAllCoroutines ();
+			StartCoroutine (ScrollTo(new Vector2 (0, position * tweetHeight)));
+		}
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			position = Mathf.Max (position - 1, 0);
+
+			StopAllCoroutines ();
+			StartCoroutine (ScrollTo(new Vector2 (0, position * tweetHeight)));
+		}
+	}
+
+	IEnumerator ScrollTo (Vector2 endPosition) {
+		Vector2 startPosition = feed.anchoredPosition;
+		float lerpTime = 0.5f;
+		float time = 0f;
+		float progress = 0f;
+
+		while (time < lerpTime) {
+			time += Time.deltaTime;
+			progress = time / lerpTime;
+
+			feed.anchoredPosition = Vector2.Lerp (startPosition, endPosition,  Mathf.Sin(progress * Mathf.PI * 0.5f));
+			yield return null;			
+		}
+
 	}
 }
