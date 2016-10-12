28c4d22cc256d7e44ed0af6dbb931593b386d1e3
various.authors <various.authors@gmail.com>
2016-10-12 12:19:33 -0700

    pressing x randomly creates a NO

--- Assets/Scripts/NONONOController.cs
+++ Assets/Scripts/NONONOController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class NONONOController : MonoBehaviour {
 
 	public float calmDownTime = 1f;
+	public GameObject NOPrefab;
 
 	private Animator animator;
 	private float quietTime = 0f;
@@ -14,13 +15,28 @@ public class NONONOController : MonoBehaviour {
 	void Start () {
 		animator = GetComponent<Animator>();
 	}
-	
+
+	private void GenerateNo () {
+
+		Vector3 screenPosition = Camera.main.ScreenToWorldPoint(
+			new Vector3(
+				Random.Range(0, Screen.width), 
+				Random.Range(0, Screen.height), 
+				Camera.main.farClipPlane / 2
+			)
+		);
+
+		Instantiate(NOPrefab, screenPosition, Quaternion.identity);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 		if (Input.GetKeyDown (KeyCode.X)) {
 			quietTime = 0f;
 			isRapidlyPressingX = true;
+
+			GenerateNo ();
 		} else {
 			quietTime += Time.deltaTime;
 
