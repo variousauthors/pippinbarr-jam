c93bfca908c94f1e160389f97f06ee3df436e62a
various.authors <various.authors@gmail.com>
2016-10-11 18:34:06 -0700

    animated the nonono guy

---
+++ Assets/Scripts/NONONOController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NONONOController : MonoBehaviour {
+
+	public float calmDownTime = 1f;
+
+	private Animator animator;
+	private float quietTime = 0f;
+
+	private bool isRapidlyPressingX = false;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.X)) {
+			quietTime = 0f;
+			isRapidlyPressingX = true;
+		} else {
+			quietTime += Time.deltaTime;
+
+			if (quietTime > calmDownTime) {
+				isRapidlyPressingX = false;
+			}
+
+		}
+
+		animator.SetBool ("isRapidlyPressingX", isRapidlyPressingX);
+	}
+}
