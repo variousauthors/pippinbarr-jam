8cd559727a67f54dd2b2fbbd1c67c372d33b3e2c
various.authors <various.authors@gmail.com>
2016-10-14 17:39:33 -0700

    added sunset

---
+++ Assets/SunsetController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunsetController : MonoBehaviour {
+
+	private int startY = 249;
+	private float period = 1f;
+	private float tic = 0f;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.localPosition = new Vector2 (0, startY);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tic += Time.deltaTime;
+
+		if (tic < period) {
+			return;
+		}
+
+		tic = 0f;
+
+		this.transform.localPosition -= new Vector3 (0, 1, 0);
+
+	}
+}
