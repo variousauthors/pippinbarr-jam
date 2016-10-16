7cac3ddbafe7df80f8db014022c89970496c2b8d
various.authors <various.authors@gmail.com>
2016-10-16 15:03:39 -0700

    added typing sounds

---
+++ Assets/AudioSourceController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSourceController : MonoBehaviour {
+
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+		audio.time = Random.Range(0, 10);
+		audio.Play ();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			audio.Stop();
+			audio.Play();
+		}
+		Debug.Log(audio.time);
+	}
+}
